f7a88c2ce0b732553cd27ece7711135576bf097e
MiKyung2 <123jmk@naver.com>
2024-08-15 22:10:58 +0900

    sidemenu style 분리

--- client/src/components/SideMenu/index.jsx
+++ client/src/components/SideMenu/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoMdMenu, IoMdClose } from "react-icons/io";
-import MenuIcon from "./icons/menu";
+import MenuIcon from "../icons/menu";
+import './style.css';
 
 function SideMenu({ imageList }) {
   const [showMenu, setShowMenu] = useState(false);
@@ -8,13 +9,22 @@ function SideMenu({ imageList }) {
     setShowMenu((prev) => !prev);
   };
 
+  const handleClickLogin = () => {
+    openLoginPopup();
+  }
+
+  function openLoginPopup() {
+
+  }
+
   return (
     <nav>
       <div className="icon-area" onClick={handleClickMenu}>
         {showMenu ? <IoMdClose /> : <MenuIcon />}
       </div>
       <div className={`menu ${showMenu ? "show" : ""}`}>
-        <div className="menu-item">저장한 이미지</div>
+        <div className="menu-item" onClick={handleClickLogin}>로그인</div>
+        {/* <div className="menu-item"> 저장한 이미지</div> */}
         <ul>
           {imageList.map(({ image, date }) => (
             <li>
